f58b4a3f3826685c591171733a1d4b601543d2f9
OscaPete <pet21048@byui.edu>
2022-01-12 16:47:06 -0700

    simplifying code

--- js/last-edited.js
+++ js/last-edited.js
@@ -1,5 +1,3 @@
 document.querySelector(".copyright").innerHTML = "&copy; " + new Date().getFullYear();
 
-var text = document.querySelector("footer p:last-child").innerText;
-
-document.querySelector("footer p:last-child").innerText = text + document.lastModified;
+document.querySelector("footer p:last-child").innerText += document.lastModified;
