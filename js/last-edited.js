97e8391992bfd1402ed7816ba826d44e036f2e4d
OscaPete <pet21048@byui.edu>
2022-01-12 16:57:11 -0700

    fixing last updated code

--- js/last-edited.js
+++ js/last-edited.js
@@ -1,3 +1,8 @@
 document.querySelector(".copyright").innerHTML = "&copy; " + new Date().getFullYear();
 
-document.querySelector("footer p:last-child").innerText += document.lastModified;
+//document.querySelector("footer p:last-child").innerText += document.lastModified;
+var p = document.createElement("p");
+
+p.innerText = "Last Modified: " + document.lastModified;
+
+document.querySelector("footer").appendChild(p);
