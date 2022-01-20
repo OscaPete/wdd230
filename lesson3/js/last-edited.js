c638f9aedc361966fce85d3378b448069f3bbe4d
OscaPete <pet21048@byui.edu>
2022-01-20 16:30:57 -0700

    created framework for site plan site

---
+++ lesson3/js/last-edited.js
@@ -0,0 +1,8 @@
+document.querySelector(".copyright").innerHTML = "&copy; " + new Date().getFullYear() + " | Oscar Peterson | Utah";
+
+//document.querySelector("footer p:last-child").innerText += document.lastModified;
+var p = document.createElement("p");
+
+p.innerText = "Last Modified: " + document.lastModified;
+
+document.querySelector("footer").appendChild(p);
